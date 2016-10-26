218391fa045184462203eec25d9142a4c8bd702e
bahri <ay@et.esiea.fr>
2016-10-26 18:18:49 +0200

    Quatrieme Manager-Task OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   def homepage 
  @departments = Department.all # on definit la methode @departments
  @tasks = Task.all # pareil mais avec les taches
- @employees = Employee.all
   end
  
 
