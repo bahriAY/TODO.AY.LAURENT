bdaa1ab31fa303407ebd4dbc6690ece3b7ba0518
bahri <ay@et.esiea.fr>
2016-11-20 14:58:59 +0100

    modi

--- db/migrate/20161024000502_create_departments.rb
+++ db/migrate/20161024000502_create_departments.rb
@@ -4,10 +4,5 @@ class CreateDepartments < ActiveRecord::Migration
       t.string :name
       t.timestamps null: false
     end
-/*  create_table :managers do |t|
-  t.belongs_to :department, index:true
-  t.string :manager_id
-  t.timestamps
-  end*/
   end
 end
